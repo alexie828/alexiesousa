ccb618efd992fb193c351f412fa6b585d00bb351
calanimagebeta <atpachec@ucsd.edu>
2016-03-29 17:35:20 -0700

    Updated content

--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,7 @@
-/**
- * DOESN'T WORK
- *
 $(document).ready(function() {
-	$('#main').click(showMain);
-	$('#about').click(showAbout);
-})
-
-$(window).scroll(function () {
-    console.log($(window).scrollTop());
-    var topDivHeight = $(".proj-container").height();
-    var viewPortSize = $(window).height();
-
-    var triggerAt = 400;
-    var triggerHeight = (topDivHeight - viewPortSize) + triggerAt;
-
-    if ($(window).scrollTop() >= triggerHeight) {
-        $('#proj1-contents').css('visibility', 'visible').hide().fadeIn();
-        $(this).off('scroll');
-    }
+    $('#name-title').bind('load', function() {
+        $(this).fadeIn('slow');
+    };
 });
 
-function showMain() {
-	$('#main').css('display', 'block');
-	$('#about').css('display', 'none');
-}
 
-function showAbout(){
-	$('#main').css('display', 'none');
-	$('#about').css('display', 'none');
-}
-*/
